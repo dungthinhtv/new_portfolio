8a2656dc9613f3668c3e76c709c3a778c8f550a2
dungthinhtv <dungthinhtv@gmail.com>
2024-05-20 19:33:34 +0700

    ss

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! Tôi là Dũng Đặng, nhà phát triển phần mềm tại Việt Nam.
           </p>
 
           <a href="#about">
